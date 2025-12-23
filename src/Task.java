e62ea72123558bc9b9db8d27d6f286cf8dacf5f1
Elena <elvishn475@gmail.com>
2025-12-23 19:53:29 +0300

    create Task class

--- src/Task.java
+++ src/Task.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
+import java.time.Instant;
 
 public class Task {
+    private static int counter = 0;
+    private int id;
+    private String title;
+    private String description;
+    private TaskStatus status;
+    private final Instant createdAt;
+
+    public Task(String title, String description) {
+        this.id = ++counter;
+        this.title = title;
+        this.description = description;
+        this.status = TaskStatus.TODO;
+        this.createdAt = Instant.now();
+
+    }
+
+    public void setTitle(String s) {
+        title = s;
+    }
+
+    public void setDescription(String d) {
+        description = d;
+    }
+
+    public void setStatus(TaskStatus st) {
+        status = st;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
 }
--- src/TaskStatus.java
+++ src/TaskStatus.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public enum TaskStatus {
+    TODO,
+    IN_PROGRESS,
+    DONE
 }
